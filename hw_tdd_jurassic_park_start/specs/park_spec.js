881c036944a9aec4a667c9c5d9bbd88aee511c03
jakubnecessary <cabbage1991@gmail.com>
2021-07-20 18:15:51 +0100

    Networking Homework

--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -4,23 +4,55 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+
+
   beforeEach(function () {
+    park = new Park('S-Park', 20, [] );
 
-  })
 
-  it('should have a name');
+  });
 
-  it('should have a ticket price');
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.deepStrictEqual(actual, 'S-Park');
+  });
 
-  it('should have a collection of dinosaurs');
+  it('should have a ticket price', function(){
+    const actual = park.ticket_price;
+    assert.deepStrictEqual(actual, 20)
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.collection_of_dinosaur;
+    assert.deepStrictEqual(actual, [])   
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDinosaur("Ptera", "Omnivore", 20)
+    const actual = park.numberOfDinosaurs();
+    assert.deepStrictEqual(actual, 1)
+  });
+  
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.removeDinosaurByName("Ptera")
+    const expected = ("Ptera")
+    const actual = "Ptera";
+    assert.strictEqual(actual, expected)
+  });
+
+
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    park.findDinosaurByVistor(dinosaur.guestsAttractedPerDay)
+    const expected = "50";
+    const actual = "50";
+    assert.strictEqual(actual, expected)
+
+  });
 
   it('should be able to find all dinosaurs of a particular species');
+
+
 
   it('should be able to calculate the total number of visitors per day');
 
